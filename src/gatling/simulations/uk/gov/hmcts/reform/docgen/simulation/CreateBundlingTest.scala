542554b6673c8c3027555f102a69b32d9b004de7
Jai Santhosh <jai.santhosh@HMCTS.NET>
2020-01-21 13:21:29 +0000

    number of user change for bundling

--- src/gatling/simulations/uk/gov/hmcts/reform/docgen/simulation/CreateBundlingTest.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/docgen/simulation/CreateBundlingTest.scala
@@ -13,7 +13,7 @@ class CreateBundlingTest extends Simulation {
 	val caseFeeder=csv("case_data.csv").circular
 
 	val httpProtocol = http
-		.proxy(Proxy("proxyout.reform.hmcts.net", 8080))
+//		.proxy(Proxy("proxyout.reform.hmcts.net", 8080))
 		.baseUrl(Environment.bundlingURL)
 
 
