c09f74c2d90611ac23865dd4a6975d5cb6c7d0e3
amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
2021-01-11 13:26:04 +0000

    Update FullTest.scala

--- src/gatling/simulations/uk/gov/hmcts/reform/docgen/simulation/FullTest.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/docgen/simulation/FullTest.scala
@@ -82,7 +82,7 @@ class FullTest extends Simulation {
 
   setUp(
     //createAnnotations_Scn.inject(rampUsers(10) during (300)).protocols(httpProtocolAnnotation),
-    createBundling_Scn.inject(rampUsers(10) during (300)).protocols(httpProtocolBundling))
+    createBundling_Scn.inject(rampUsers(1) during (30)).protocols(httpProtocolBundling))
     //DMStore_Scn.inject(rampUsers(10) during (300)).protocols(httpProtocolDMStore),
     //docAssembly_Scn.inject(rampUsers(10) during (300)).protocols(httpProtocolDocAssembly))
   //.maxDuration(90 minutes)
